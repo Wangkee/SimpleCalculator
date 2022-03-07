d687d3abd0f3ada1ff5d91e03fd3da97a5df983a
Wangkee <38874807+Wangkee@users.noreply.github.com>
2022-03-07 12:14:31 +0800

    Update myFrame.java

--- myFrame.java
+++ myFrame.java
@@ -1,21 +1,33 @@
 package CalculatorProject;
 
-import javax.script.ScriptException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class myFrame extends JFrame {
+
+
+    /**
+     * panel : 设置面板
+     * label : 计算器显示屏
+     * buttons : 按钮数组
+     * names : 按钮名数组
+     */
     private JPanel panel;
-    private JButton [] buttons = new JButton[20];
     private JLabel label;
-
-    private StringBuilder curNum = new StringBuilder("0");
+    private JButton [] buttons = new JButton[25];
+    private final String [] names = {"AC", "DEL", "^", "=", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+",
+                              "(", ")", "0", "/"};
+    /**
+     * curNum : 存储当前的表达式
+     * op : 操作码，当前curNum是结果，如果是按下其他按钮时要先清空
+     * calculator : 计算器实例，用于计算表达式结果
+     */
     private int op = 0;
-
+    private StringBuilder curNum = new StringBuilder("0");
     private calculator c = new calculator();
+
 
     public myFrame(String title) {
         super(title);
@@ -35,39 +47,20 @@ public class myFrame extends JFrame {
         label.setOpaque(true);
         label.setBackground(new Color(255,255,255));
         label.setBounds(20,20,350,170);
-        label.setHorizontalAlignment(SwingConstants.SOUTH_EAST);
+        label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setFont(new Font("微软雅黑",Font.BOLD,40));
         label.setText(curNum.toString());
         panel.add(label);
     }
 
     public void setButtons(){
-        int cnt = 0;
-        buttons[cnt++] = new JButton("AC");
-        buttons[cnt++] = new JButton("DEL");
-        buttons[cnt++] = new JButton("^");
-        buttons[cnt++] = new JButton("/");
-        buttons[cnt++] = new JButton("7");
-        buttons[cnt++] = new JButton("8");
-        buttons[cnt++] = new JButton("9");
-        buttons[cnt++] = new JButton("x");
-        buttons[cnt++] = new JButton("4");
-        buttons[cnt++] = new JButton("5");
-        buttons[cnt++] = new JButton("6");
-        buttons[cnt++] = new JButton("-");
-        buttons[cnt++] = new JButton("1");
-        buttons[cnt++] = new JButton("2");
-        buttons[cnt++] = new JButton("3");
-        buttons[cnt++] = new JButton("+");
-        buttons[cnt++] = new JButton("(");
-        buttons[cnt++] = new JButton(")");
-        buttons[cnt++] = new JButton("0");
-        buttons[cnt++] = new JButton("=");
-
-        System.out.println(cnt);
+        for(int i = 0 ; i < names.length ; i++){
+            buttons[i] = new JButton(names[i]);
+        }
 
         int x = 20, y = 200;
-        for (int i = 0; i < cnt; i++) {
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(i);
             JButton button = buttons[i];
 
             if(x +90 > 400){
@@ -83,24 +76,7 @@ public class myFrame extends JFrame {
     }
 
     public void setListener(){
-        //等于
-        buttons[19].addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                c.getExpression(curNum.toString());
-                String res = c.getRes();
-                curNum = new StringBuilder(res);
-                op = 1;
-                if(curNum.length() > 14){
-                    label.setFont(new Font("微软雅黑",Font.BOLD,20));
-                }else{
-                    label.setFont(new Font("微软雅黑",Font.BOLD,40));
-                }
-                label.setText(curNum.toString());
-            }
-        });
-
-        //AC
+        //AC按钮
         buttons[0].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -109,7 +85,7 @@ public class myFrame extends JFrame {
             }
         });
 
-        //退格
+        //退格按钮
         buttons[1].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -118,184 +94,39 @@ public class myFrame extends JFrame {
                     if(curNum.length() == 0){
                         curNum.append("0");
                     }
-                    if(curNum.length() > 14){
-                        label.setFont(new Font("微软雅黑",Font.BOLD,20));
-                    }else{
-                        label.setFont(new Font("微软雅黑",Font.BOLD,40));
-                    }
+                    setFontChange();
                     label.setText(curNum.toString());
                 }
 
             }
         });
 
-        //加法
-        buttons[15].addActionListener(new ActionListener() {
+        //等于按钮
+        buttons[3].addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                pressOperatorButton("+");
-            }
-        });
-
-        //减法
-        buttons[11].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressOperatorButton("-");
-            }
-        });
-
-        //乘法
-        buttons[7].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressOperatorButton("*");
-            }
-        });
-
-        //除法
-        buttons[3].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressOperatorButton("/");
-            }
-        });
-
-        //数字7
-        buttons[4].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(7);
-            }
-        });
-
-        //数字8
-        buttons[5].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(8);
-            }
-        });
-
-        //数字9
-        buttons[6].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(9);
-            }
-        });
-
-        //数字4
-        buttons[8].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(4);
-            }
-        });
-
-        //数字5
-        buttons[9].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(5);
-            }
-        });
-
-        //数字6
-        buttons[10].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(6);
-            }
-        });
-
-        //数字1
-        buttons[12].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(1);
-            }
-        });
-
-        //数字2
-        buttons[13].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(2);
-            }
-        });
-
-        //数字3
-        buttons[14].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(3);
-            }
-        });
-
-        //数字0
-        buttons[18].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pressNumButton(0);
-            }
-        });
-
-        //左括号
-        buttons[16].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(curNum.charAt(0) == '0'){
-                    curNum.replace(0,1,"(");
-                }else {
-                    curNum.append("(");
-                }
-                if(curNum.length() > 14){
-                    label.setFont(new Font("微软雅黑",Font.BOLD,20));
-                }else{
-                    label.setFont(new Font("微软雅黑",Font.BOLD,40));
-                }
+                System.out.println(curNum.toString());
+                c.getExpression(curNum.toString());
+                String res = c.getRes();
+                curNum = new StringBuilder(res);
+                op = 1;
+                setFontChange();
                 label.setText(curNum.toString());
             }
         });
 
-        //右括号
-        buttons[17].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(curNum.charAt(0) == '0'){
-                    curNum.replace(0,1,")");
-                }else {
-                    curNum.append(")");
+        //其他按钮
+        for (int i = 4; i < names.length; i++) {
+            JButton button = buttons[i];
+            String str = names[i];
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    pressOperatorButton(str);
                 }
-                if(curNum.length() > 14){
-                    label.setFont(new Font("微软雅黑",Font.BOLD,20));
-                }else{
-                    label.setFont(new Font("微软雅黑",Font.BOLD,40));
-                }
-                label.setText(curNum.toString());
-            }
-        });
-    }
-
-    public void pressNumButton(int num){
-        if(op == 1){
-            curNum = new StringBuilder("0");
-            op = 0;
+            });
         }
-
-        if(curNum.charAt(0) == '0'){
-            curNum.replace(0,1,String.valueOf(num));
-        }else {
-            curNum.append(String.valueOf(num));
-        }
-        if(curNum.length() > 14){
-            label.setFont(new Font("微软雅黑",Font.BOLD,20));
-        }else{
-            label.setFont(new Font("微软雅黑",Font.BOLD,40));
-        }
-        label.setText(curNum.toString());
-    }
+   }
 
     public void pressOperatorButton(String operator){
         if(op == 1){
@@ -305,14 +136,29 @@ public class myFrame extends JFrame {
             return;
         }
 
-        if(Character.isDigit(curNum.charAt(curNum.length()-1))){
-            curNum.append(operator);
+        if(operator.equals("+") || operator.equals("-") || operator.equals("x") || operator.equals("/")){
+            if(Character.isDigit(curNum.charAt(curNum.length()-1)) || curNum.charAt(curNum.length()-1) == ')'){
+                if(operator.equals("x")) curNum.append("*");
+                else curNum.append(operator);
+            }
+        }else{
+            if (curNum.charAt(0) == '0' && curNum.length() == 1) {
+                curNum.replace(0, 1, operator);
+            } else {
+                curNum.append(operator);
+            }
         }
+
+        setFontChange();
+
+        label.setText(curNum.toString());
+    }
+
+    public void setFontChange(){
         if(curNum.length() > 14){
             label.setFont(new Font("微软雅黑",Font.BOLD,20));
         }else{
             label.setFont(new Font("微软雅黑",Font.BOLD,40));
         }
-        label.setText(curNum.toString());
     }
 }
